7a2c3fe877479145e7a5e462a501792201d31d26
Nixeff <theodore.folkesson@gmail.com>
2025-03-18 20:22:53 +0100

    Stuff done
    Grid created, Check row if blocked done and remove row of body parts is
    complete

--- Tetris/src/tetris/GridBlock.java
+++ Tetris/src/tetris/GridBlock.java
@@ -1,10 +1,41 @@
 package tetris;
 
 public class GridBlock {
-	int x;
-	int y;
-	public GridBlock(int x, int y) {
+	private int x;
+	private int y;
+	private int width;
+	private boolean blocked = false;
+	
+	/**
+	 * @param x
+	 * @param y
+	 * @param width
+	 */
+	public GridBlock(int x, int y, int width) {
 		this.x = x;
 		this.y = y;
+		this.width = width;
 	}
+	
+
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public boolean isBlocked() {
+		return blocked;
+	}
+	public void setBlocked(boolean blocked) {
+		this.blocked = blocked;
+	}
+	
+	
 }
--- Tetris/src/tetris/Tetromino.java
+++ Tetris/src/tetris/Tetromino.java
@@ -1,5 +1,12 @@
 package tetris;
 
+import java.util.ArrayList;
+
 public class Tetromino {
 
+	ArrayList<Block> bodyPieces;
+
+	public ArrayList<Block> getBodyPieces() {
+		return bodyPieces;
+	}
 }
