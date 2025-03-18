686652e9890d39ae16ffd948b01ee7d1a8b86fa3
Nixeff <theodore.folkesson@gmail.com>
2025-03-18 19:31:41 +0100

    Grid is created

--- Tetris/src/tetris/Gameboard.java
+++ Tetris/src/tetris/Gameboard.java
@@ -1,5 +1,19 @@
 package tetris;
 
+import java.util.ArrayList;
+
 public class Gameboard {
 
+	public Gameboard() {
+		int height = 22;
+		int width = 10;
+		ArrayList<ArrayList<GridBlock>> grid = new ArrayList<ArrayList<GridBlock>>();
+		
+		for (int i = 0; i < height; i++ ) {
+			grid.add(new ArrayList<GridBlock>());
+			for (int o = 0; o < width; o++ ) {
+				grid.get(i).add(new GridBlock(o,i));
+			}
+		}
+	}
 }
--- Tetris/src/tetris/GridBlock.java
+++ Tetris/src/tetris/GridBlock.java
@@ -1,5 +1,10 @@
 package tetris;
 
 public class GridBlock {
-
+	int x;
+	int y;
+	public GridBlock(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 }
